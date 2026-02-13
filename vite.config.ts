f7593ff779c4d3c701f83001dd0319076ce7c451
SimonStockinger <simo.stockinger@gmail.com>
2026-02-13 08:07:11 +0100

    Updated vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path";
 
-// https://vite.dev/config/
-
 export default defineConfig({
-//  base: "/study-planner/",
+  base: "/METIS",
   plugins: [
     react({
       babel: {
